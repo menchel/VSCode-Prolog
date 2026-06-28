530d58ca9c4a01b1058d312ed298aae745cad31a
Inbar Menchel <118845141+menchel@users.noreply.github.com>
2026-06-28 17:19:27 +0300

    Change git remote and sparse checkout configuration
    Updated remote repository and sparse checkout paths for new semester.

--- .devcontainer/fetch-git.sh
+++ .devcontainer/fetch-git.sh
@@ -2,8 +2,7 @@ git config --global core.eol lf
 git config --global core.autocrlf input
 git init hw_files
 cd hw_files
-git remote add -f origin https://github.com/OpenUniversity/PL-236319-Spr-2024.git
-git config core.sparseCheckout true 
-echo "Spr24/HW/Homework5/" > .git/info/sparse-checkout 
-echo "Spr24/HW/Homework6/" >> .git/info/sparse-checkout 
-git pull origin master
+git remote add -f origin https://github.com/Technion-Programming-Languages/PL-236319-Spr-2026.git
+git config core.sparseCheckout true #
+echo "Spr26/HW/Homework 5/" >> .git/info/sparse-checkout # 
+git pull origin master #
